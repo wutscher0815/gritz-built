07816c82a31393e23b634ca78f4b0456784aa4c8
Hartwig Wutscher <wutscher0815@gmail.com>
2019-10-14 22:43:18 +0200

    slkcjalksjcalcx

---
+++ precache-manifest.65c8c12d539b61c0aa13240aeb3dea23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad2c48c726a216b95875cd95075a8cc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d0b3e9d3fda688c3037",
+    "url": "/static/css/main.f30fab5c.chunk.css"
+  },
+  {
+    "revision": "14c2b35f2dd073115b70",
+    "url": "/static/js/2.38e71e83.chunk.js"
+  },
+  {
+    "revision": "3d0b3e9d3fda688c3037",
+    "url": "/static/js/main.9a66341c.chunk.js"
+  },
+  {
+    "revision": "5c6baf24a285ea60dbc5",
+    "url": "/static/js/runtime~main.1326826e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
